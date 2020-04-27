b0e62f26b22c7b08be2451d621f2d7a57a3f947b
Kashishdwn <kashishdwn@gmail.com>
2020-04-27 22:30:06 +0530

    Updates

---
+++ precache-manifest.f6f34488c85aeb74b31deee5fa70b1a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ebdf274e0f15f28ec8bf08205fe22f8",
+    "url": "/Color-Boxes/index.html"
+  },
+  {
+    "revision": "32aaa1101864b2efb7c3",
+    "url": "/Color-Boxes/static/css/main.81202e9c.chunk.css"
+  },
+  {
+    "revision": "8a12d7d3b22f5e90e671",
+    "url": "/Color-Boxes/static/js/2.dabbbd1e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Color-Boxes/static/js/2.dabbbd1e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32aaa1101864b2efb7c3",
+    "url": "/Color-Boxes/static/js/main.62b01c83.chunk.js"
+  },
+  {
+    "revision": "829cdaa3b682a5157bad",
+    "url": "/Color-Boxes/static/js/runtime-main.a7dacfd4.js"
+  }
+]);
